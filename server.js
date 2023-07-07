ca09cb752936e211754d1693ea91f51c2c8ce272
AnkaDemetria <116574892+AnkaDemetria@users.noreply.github.com>
2023-07-07 10:42:06 +0200

    SSL commit

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const app = express();
 const errorHandler = require("_middleware/error-handler");
 const port = 3004;
 
+/* On crée les constantes pour utiliser le serveur https */
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+ 
+/* On récupère notre clé privée et notre certificat (ici ils se trouvent dans le dossier certificate) */
+const key = fs.readFileSync(path.join(__dirname, 'certificate', 'server.key'));
+const cert = fs.readFileSync(path.join(__dirname, 'certificate', 'server.cert'));
+ 
+const options = { key, cert };
+ 
+/* Puis on crée notre serveur HTTPS */
+https.createServer(options, app).listen(8080, () => {
+  console.log('App is running ! Go to https://localhost:8080');
+});
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
